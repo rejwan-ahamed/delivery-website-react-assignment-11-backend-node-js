addc327adf71a911d75eb49c2e863ea21e45149f
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-09 11:12:38 +0600

    user name and password is encrypted

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
@@ -17,7 +18,7 @@ app.get('/',(req,res)=>{
 
 // mongo db 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.vhnnfcc.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${env.process.DB_User}:${env.process.DB_Password}@cluster0.vhnnfcc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
