16daed00917735149e59cd06f5a4219e0edd6e65
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-09 11:09:05 +0600

    env added

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
+// middle ware
 app.use(cors());
 app.use(express.json())
+
+// get api
+app.get('/',(req,res)=>{
+    res.send('get api is working')
+})
+
+
+// mongo db 
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.vhnnfcc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+// api listening
+app.listen(port,()=>{
+    console.log(`api listening port is ${port}`)
+})
+
