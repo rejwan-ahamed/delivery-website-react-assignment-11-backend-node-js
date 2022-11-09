335905500b45547072423975a3ab6c23c154aee7
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-09 11:38:00 +0600

    get api added

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middle ware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // get api
-app.get('/',(req,res)=>{
-    res.send('get api is working')
-})
+app.get("/", (req, res) => {
+  res.send("get api is working");
+});
 
-
-// mongo db 
-
-const uri = `mongodb+srv://${env.process.DB_User}:${env.process.DB_Password}@cluster0.vhnnfcc.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
+// mongo db
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.vhnnfcc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const productCollections = client
+      .db("User_main")
+      .collection("ProductsDetails");
+    // get all products api
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const curser = productCollections.find(query);
+      const result = await curser.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => {
+  console.log(error);
+});
 
 // api listening
-app.listen(port,()=>{
-    console.log(`api listening port is ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`api listening port is ${port}`);
+});
